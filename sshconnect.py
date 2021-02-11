e08f0a308b30c254fd3997bb163eb6bc645123d7
ankurgyani <arg58@njit.edu>
2021-02-11 01:18:39 -0500

    test connection to sftp server

---
+++ sshconnect.py
@@ -0,0 +1,11 @@
+import paramiko
+ssh_client=paramiko.SSHClient()
+ssh_client.load_system_host_keys()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname="localhost", username="foo",password="pass", port=2222)
+sftp_client=ssh_client.open_sftp()
+# sftp_client.chdir("/home/foo/upload/customer_event_ids.json")
+sftp_client.mkdir("upload2021",mode="0777")
+#sftp_client.put("customer_event_ids.json","/upload/customer_event_ids.json")
+sftp_client.close()
+ssh_client.close()
